1f1081ded1ce80c88f32d74d0231326bb449ecca
ptlmeet27 <20124040@nuv.ac.in>
2022-01-20 00:42:02 +0530

    ADD: redux for cars

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
+
 import Header from './components/Header';
 import Home from './components/Home';
 import styled from 'styled-components';
 function App() {
   return (
     <div className="App">
-      {/* <Header /> */}
+      <Header />
       <Home />
     </div>
   );
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,28 +1,32 @@
 import React from "react";
 import styled from "styled-components";
+import Fade from 'react-reveal/Fade';
 
 function Section({title, description, leftBtnText, rightBtnText, backgroundImg}) {
     return (
         <Wrap bgImage={backgroundImg}>
-            <ItemText >
-                <h1>{ title }</h1>
-                <p> {description} </p>{" "}
-            </ItemText>
-            <Buttons>
-                <ButtonGroup>
-                    <LeftButton>
-                        {leftBtnText}
-                    </LeftButton>
+            <Fade bottom>
+                <ItemText >
+                    <h1>{ title }</h1>
+                    <p> {description} </p>{" "}
+                </ItemText>
+            </Fade>
+                <Buttons>
+                <Fade bottom>
+                    <ButtonGroup>
+                        <LeftButton>
+                            {leftBtnText}
+                        </LeftButton>
 
-                    {rightBtnText && 
-                        <RightButton>
-                            {rightBtnText}
-                        </RightButton>
-                    } 
-                </ButtonGroup>
+                        {rightBtnText && 
+                            <RightButton>
+                                {rightBtnText}
+                            </RightButton>
+                        } 
+                    </ButtonGroup>
+                </Fade>
                 <DownArrow src="/images/down-arrow.svg" />
-            </Buttons>
-            
+                </Buttons>
         </Wrap>
     );
 }
@@ -30,21 +34,23 @@ function Section({title, description, leftBtnText, rightBtnText, backgroundImg})
 export default Section;
 
 const Wrap = styled.div`
-  width: 100vw;
-  height: 100vh;
-  background-size: cover;
-  background-position: center;
-  background-repeat: no-repeat;
-  ${'' /* background-image: url("/images/model-s.jpg"); */}
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: center;
-  background-image: ${props => `url("/images/${props.bgImage}")`};
+    z-index:10;
+    width: 100vw;
+    height: 100vh;
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    ${'' /* background-image: url("/images/model-s.jpg"); */}
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    background-image: ${props => `url("/images/${props.bgImage}")`};
 `
 const ItemText = styled.div`
     padding-top: 15vh;
     text-align:center;
+    z-index:10;
 `
 
 const ButtonGroup = styled.div`
