11980f819610633c1276f25e9dad10e31dde5fea
ptlmeet27 <20124040@nuv.ac.in>
2022-01-20 02:28:29 +0530

    master

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,49 +8,49 @@ function Home() {
       <Section
         title="Model S"
         description="Order Online for Touchless Delivery"
-        backgroundImg="../model-s.jpg"
+        backgroundImg="/public/images/model-s.jpg"
         leftBtnText="Custom Order"
         rightBtnText="Existing inventory"
       />
       <Section
         title="Model Y"
         description="Order Online for Touchless Delivery"
-        backgroundImg="./public/images/model-y.jpg"
+        backgroundImg="/public/images/model-y.jpg"
         leftBtnText="Custom Order"
         rightBtnText="Existing inventory"
       />
       <Section
         title="Model 3"
         description="Order Online for Touchless Delivery"
-        backgroundImg="model-3.jpg"
+        backgroundImg="/public/images/model-3.jpg"
         leftBtnText="Custom Order"
         rightBtnText="Existing inventory"
       />
       <Section
         title="Model X"
         description="Order Online for Touchless Delivery"
-        backgroundImg="model-x.jpg"
+        backgroundImg="/public/images/model-x.jpg"
         leftBtnText="Custom Order"
         rightBtnText="Existing inventory"
       />
        <Section
         title="Lowest Cost Solar Panels in America"
         description="Money-back guarantee"
-        backgroundImg="solar-panel.jpg"
+        backgroundImg="/public/images/solar-panel.jpg"
         leftBtnText="Order now"
         rightBtnText="Learn more"
       />
       <Section
         title="Solar Roof"
         description="Produce Clean Energy From Your Roof"
-        backgroundImg="solar-roof.jpg"
+        backgroundImg="/public/images/solar-roof.jpg"
         leftBtnText="Order now"
         rightBtnText="Learn more"
       />
       <Section
         title="Accessories"
         description=" "
-        backgroundImg="accessories.jpg"
+        backgroundImg="/public/images/accessories.jpg"
         leftBtnText="Shop now"
       />
 
