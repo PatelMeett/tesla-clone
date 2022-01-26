5b8f942a16c5401c92661ce3a84743325a7b0778
ptlmeet27 <20124040@nuv.ac.in>
2022-01-26 15:59:05 +0530

    initial changes

--- src/components/Section.js
+++ src/components/Section.js
@@ -25,7 +25,7 @@ function Section({title, description, leftBtnText, rightBtnText, backgroundImg})
                         } 
                     </ButtonGroup>
                 </Fade>
-                <DownArrow src="/images/down-arrow.svg" />
+                <DownArrow src="./images/down-arrow.svg" />
                 </Buttons>
         </Wrap>
     );
